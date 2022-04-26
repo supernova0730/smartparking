72f372dfc251eb96b2ad4bc45753f1dcbd22375d
supernova0730 <kuanysheveldar123@gmail.com>
2022-04-26 15:39:48 +0600

    added yaml tags

--- config/config.go
+++ config/config.go
@@ -18,56 +18,56 @@ var (
 )
 
 type Config struct {
-	DB         DB         `mapstructure:"db"`
-	Cache      Cache      `mapstructure:"cache"`
-	Web        Web        `mapstructure:"web"`
-	JWT        JWT        `mapstructure:"jwt"`
-	Email      Email      `mapstructure:"email"`
-	Recognizer Recognizer `mapstructure:"recognizer"`
-	ConfigPath string     `mapstructure:"config_path"`
-	HashSalt   string     `mapstructure:"hash_salt"`
+	DB         DB         `mapstructure:"db" yaml:"db"`
+	Cache      Cache      `mapstructure:"cache" yaml:"cache"`
+	Web        Web        `mapstructure:"web" yaml:"web"`
+	JWT        JWT        `mapstructure:"jwt" yaml:"jwt"`
+	Email      Email      `mapstructure:"email" yaml:"email"`
+	Recognizer Recognizer `mapstructure:"recognizer" yaml:"recognizer"`
+	ConfigPath string     `mapstructure:"config_path" yaml:"config_path"`
+	HashSalt   string     `mapstructure:"hash_salt" yaml:"hash_salt"`
 }
 
 type DB struct {
-	Host     string `mapstructure:"host"`
-	Port     string `mapstructure:"port"`
-	Name     string `mapstructure:"name"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
-	SSLMode  string `mapstructure:"sslmode"`
+	Host     string `mapstructure:"host" yaml:"host"`
+	Port     string `mapstructure:"port" yaml:"port"`
+	Name     string `mapstructure:"name" yaml:"name"`
+	Username string `mapstructure:"username" yaml:"username"`
+	Password string `mapstructure:"password" yaml:"password"`
+	SSLMode  string `mapstructure:"sslmode" yaml:"sslmode"`
 }
 
 type Cache struct {
-	Host string        `mapstructure:"host"`
-	Port string        `mapstructure:"port"`
-	TTL  time.Duration `mapstructure:"ttl"`
+	Host string        `mapstructure:"host" yaml:"host"`
+	Port string        `mapstructure:"port" yaml:"port"`
+	TTL  time.Duration `mapstructure:"ttl" yaml:"ttl"`
 }
 
 type Web struct {
-	Host        string `mapstructure:"host"`
-	Port        string `mapstructure:"port"`
-	TLSEnable   bool   `mapstructure:"tlsenable"`
-	CertFile    string `mapstructure:"certfile"`
-	KeyFile     string `mapstructure:"keyfile"`
-	FileStorage string `mapstructure:"file_storage"`
+	Host        string `mapstructure:"host" yaml:"host"`
+	Port        string `mapstructure:"port" yaml:"port"`
+	TLSEnable   bool   `mapstructure:"tlsenable" yaml:"tlsenable"`
+	CertFile    string `mapstructure:"certfile" yaml:"certfile"`
+	KeyFile     string `mapstructure:"keyfile" yaml:"keyfile"`
+	FileStorage string `mapstructure:"file_storage" yaml:"file_storage"`
 }
 
 type JWT struct {
-	SecretKey       string        `mapstructure:"secret"`
-	AccessTokenTTL  time.Duration `mapstructure:"access_token_ttl"`
-	RefreshTokenTTL time.Duration `mapstructure:"refresh_token_ttl"`
+	SecretKey       string        `mapstructure:"secret" yaml:"secret"`
+	AccessTokenTTL  time.Duration `mapstructure:"access_token_ttl" yaml:"access_token_ttl"`
+	RefreshTokenTTL time.Duration `mapstructure:"refresh_token_ttl" yaml:"refresh_token_ttl"`
 }
 
 type Email struct {
-	Sender   string `mapstructure:"sender"`
-	Password string `mapstructure:"password"`
-	SMTPHost string `mapstructure:"smtp_host"`
-	SMTPPort string `mapstructure:"smtp_port"`
+	Sender   string `mapstructure:"sender" yaml:"sender"`
+	Password string `mapstructure:"password" yaml:"password"`
+	SMTPHost string `mapstructure:"smtp_host" yaml:"smtp_host"`
+	SMTPPort string `mapstructure:"smtp_port" yaml:"smtp_port"`
 }
 
 type Recognizer struct {
-	URL   string `mapstructure:"url"`
-	Token string `mapstructure:"token"`
+	URL   string `mapstructure:"url" yaml:"url"`
+	Token string `mapstructure:"token" yaml:"token"`
 }
 
 func (w Web) String() string {
